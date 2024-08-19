feedcb4cc71b582eec5346ee992d0691f6037c11
planit <planit@tanjeffreyz.github.io>
2024-08-19 04:37:59 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,5 +1,21 @@
 assignments = {
   "ME30801": [],
-  "ME315 (Lecture)": [],
-  "wl.202510.ME.58100.WNG": []
+  "ME315 (Lecture)": [
+    {
+      "title": "Homework 1",
+      "course": "ME315 (Lecture)",
+      "dueDate": "2024-08-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/814076",
+      "submitted": false
+    }
+  ],
+  "wl.202510.ME.58100.WNG": [
+    {
+      "title": "Homework-0, due 8/27",
+      "course": "wl.202510.ME.58100.WNG",
+      "dueDate": "2024-08-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/821837",
+      "submitted": false
+    }
+  ]
 };
