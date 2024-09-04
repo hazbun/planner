2f1ef37af49cfe7c1e19aca99354abb788e1828d
planit <planit@tanjeffreyz.github.io>
2024-09-04 19:36:07 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -31,24 +31,24 @@ assignments = {
       "submitted": false
     }
   ],
-  "ME315Lab - Div 051": [
+  "ME315Lab - Div 051 (Fri 09:30)": [
     {
       "title": "Prelab-1",
-      "course": "ME315Lab - Div 051",
+      "course": "ME315Lab - Div 051 (Fri 09:30)",
       "dueDate": "2024-09-05T23:59:00",
       "link": "https://www.gradescope.com/courses/849003/assignments/4853714/submissions/263953675",
       "submitted": true
     },
     {
       "title": "Lab-1 report",
-      "course": "ME315Lab - Div 051",
+      "course": "ME315Lab - Div 051 (Fri 09:30)",
       "dueDate": "2024-09-12T23:59:00",
       "link": "https://www.gradescope.com/courses/849003",
       "submitted": false
     },
     {
       "title": "Lab-0 Report",
-      "course": "ME315Lab - Div 051",
+      "course": "ME315Lab - Div 051 (Fri 09:30)",
       "dueDate": "2024-09-05T23:59:00",
       "link": "https://www.gradescope.com/courses/849003/assignments/4853716/submissions/263870421",
       "submitted": true
