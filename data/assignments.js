873ab4d18f3a25308f34714c026bf42536460bfa
planit <planit@tanjeffreyz.github.io>
2024-09-11 15:38:26 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -50,8 +50,8 @@ assignments = {
       "title": "Homework 3",
       "course": "ME315 (Lecture)",
       "dueDate": "2024-09-11T23:59:00",
-      "link": "https://www.gradescope.com/courses/814076",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/814076/assignments/4857784/submissions/266271357",
+      "submitted": true
     },
     {
       "title": "Homework 4",
