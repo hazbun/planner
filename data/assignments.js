69f0a530e3a425e540d15ce84eafda7b2aa9e003
planit <planit@tanjeffreyz.github.io>
2024-09-15 02:07:20 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -36,6 +36,13 @@ assignments = {
       "dueDate": "2024-09-13T23:59:00",
       "link": "https://www.gradescope.com/courses/827568/assignments/4906106/submissions/267294677",
       "submitted": true
+    },
+    {
+      "title": "Lab 3 Report",
+      "course": "ME30801",
+      "dueDate": "2024-09-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/827568",
+      "submitted": false
     }
   ],
   "ME315 (Lecture)": [
