edf9da36ee0ecb35495326a46e7b5be8fe663cb8
planit <planit@tanjeffreyz.github.io>
2024-09-16 04:40:52 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -73,6 +73,13 @@ assignments = {
       "dueDate": "2024-09-18T23:59:00",
       "link": "https://www.gradescope.com/courses/814076",
       "submitted": false
+    },
+    {
+      "title": "Homework 5 (Not Graded)",
+      "course": "ME315 (Lecture)",
+      "dueDate": "2024-09-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/814076",
+      "submitted": false
     }
   ],
   "ME315Lab - Div 051 (Fri 09:30)": [
