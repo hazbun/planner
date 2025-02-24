f4212d01bd7446f1a98cc2adbd0f68d5a0aa26d7
planit <planit@tanjeffreyz.github.io>
2025-02-24 13:40:45 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,6 +1,13 @@
 assignments = {
   "Micromechanics of Materials (LEC) ": [
     {
+      "title": "Analysis Assignment 4",
+      "course": "Micromechanics of Materials (LEC) ",
+      "dueDate": "2025-03-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/940672",
+      "submitted": false
+    },
+    {
       "title": "Analysis Assignment 3",
       "course": "Micromechanics of Materials (LEC) ",
       "dueDate": "2025-03-09T23:59:00",
